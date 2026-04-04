9651ac1227feafa319cc49af4981e25c78e06204
Swagtoy <me@ow.swag.toys>
2026-04-04 16:42:05 -0400

    Start gnome-session-service with a wrapper; cleanup/fix leader
    Signed-off-by: Swagtoy <me@ow.swag.toys>

--- src/leader-openrc.c
+++ src/leader-openrc.c
@@ -30,7 +30,6 @@ typedef struct {
         GDBusConnection *session_bus;
         GMainLoop *loop;
         int fifo_fd;
-        GDBusProxy *awaiting_shutdown;
 } Leader;
 
 static void
@@ -39,7 +38,6 @@ leader_clear (Leader *ctx)
         g_clear_object (&ctx->session_bus);
         g_clear_pointer (&ctx->loop, g_main_loop_unref);
         g_close (ctx->fifo_fd, NULL);
-        g_clear_object (&ctx->awaiting_shutdown);
 }
 
 G_DEFINE_AUTO_CLEANUP_CLEAR_FUNC (Leader, leader_clear);
@@ -72,99 +70,12 @@ leader_term_or_int_signal_cb (gpointer data)
         return G_SOURCE_REMOVE;
 }
 
-static void
-graphical_session_pre_state_changed_cb (GDBusProxy  *proxy,
-                                        GVariant    *changed_properties,
-                                        char       **invalidated_properties,
-                                        gpointer     data)
-{
-        Leader *ctx = data;
-        g_autoptr (GVariant) value = NULL;
-        const char *state;
-
-        value = g_variant_lookup_value (changed_properties, "ActiveState", NULL);
-        if (value == NULL)
-                return;
-        g_variant_get (value, "&s", &state);
-
-        if (g_strcmp0 (state, "inactive") == 0) {
-                g_debug ("Session services now inactive, quitting");
-                g_main_loop_quit (ctx->loop);
-        }
-}
-
-// TODO purge this code
 static gboolean
 monitor_hangup_cb (int          fd,
                    GIOCondition condition,
                    gpointer     user_data)
 {
-        Leader *ctx = user_data;
-        g_autoptr (GVariant) unit = NULL;
-        const char *unit_path = NULL;
-        g_autoptr (GDBusProxy) proxy = NULL;
-        g_autoptr (GVariant) value = NULL;
-        g_autoptr (GError) error = NULL;
-
-        g_debug ("Services have begun stopping, waiting for them to finish stopping");
-
-        unit = g_dbus_connection_call_sync (ctx->session_bus,
-                                            "org.freedesktop.systemd1",
-                                            "/org/freedesktop/systemd1",
-                                            "org.freedesktop.systemd1.Manager",
-                                            "GetUnit",
-                                            g_variant_new ("(s)", "graphical-session-pre.target"),
-                                            G_VARIANT_TYPE ("(o)"),
-                                            G_DBUS_CALL_FLAGS_NONE,
-                                            -1,
-                                            NULL,
-                                            &error);
-        if (!unit) {
-                g_warning ("Could not get unit for graphical-session-pre.target: %s", error->message);
-                g_main_loop_quit (ctx->loop);
-                return G_SOURCE_REMOVE;
-        }
-
-        g_variant_get (unit, "(&o)", &unit_path);
-
-        proxy = g_dbus_proxy_new_sync (ctx->session_bus,
-                                       G_DBUS_PROXY_FLAGS_NONE,
-                                       NULL,
-                                       "org.freedesktop.systemd1",
-                                       unit_path,
-                                       "org.freedesktop.systemd1.Unit",
-                                       NULL,
-                                       &error);
-        if (!proxy) {
-                g_warning ("Could not get proxy for graphical-session-pre.target unit: %s", error->message);
-                g_main_loop_quit (ctx->loop);
-                return G_SOURCE_REMOVE;
-        }
-
-        value = g_dbus_proxy_get_cached_property (proxy, "ActiveState");
-
-        if (value) {
-                const char *state;
-
-                g_variant_get (value, "&s", &state);
-
-                if (g_strcmp0 (state, "inactive") == 0) {
-                        g_debug ("State of graphical-session-pre.target unit already inactive quitting");
-                        g_main_loop_quit (ctx->loop);
-                        return G_SOURCE_REMOVE;
-                }
-                g_debug ("State of graphical-session-pre.target unit is '%s', waiting for it to go inactive", state);
-        } else {
-                g_debug ("State of graphical-session-pre.target unit is unknown, waiting for it to go inactive");
-        }
-
-        g_signal_connect (proxy,
-                          "g-properties-changed",
-                          G_CALLBACK (graphical_session_pre_state_changed_cb),
-                          ctx);
-
-        ctx->awaiting_shutdown = g_steal_pointer (&proxy);
-
+        g_main_loop_quit (((Leader *)user_data)->loop);
         return G_SOURCE_REMOVE;
 }
 
@@ -178,39 +89,8 @@ debug_logger (gchar const *log_domain,
         syslog (LOG_INFO, "%s", message);
 }
 
-/**
- * This is the session leader, i.e. it is the only process that's not managed
- * by the systemd user instance. This process is the one executed by GDM, and
- * it is part of the session scope in the system systemd instance. This process
- * works in conjunction with a service running within the user's service manager
- * (i.e. `gnome-session-monitor.service`) to implement the following:
- *
- * - When asked to shut down cleanly (i.e. via SIGTERM), the leader needs to
- *   bring down the session
- * - The leader needs to quit right after the session shuts down
- * - If the leader shuts down uncleanly, the session needs to shut down as well.
- *
- * This is achieved by opening a named FIFO in a well known location. If this
- * process receives SIGTERM or SIGINT then it will write a single byte, causing
- * the monitor service to signal STOPPING=1 to systemd. This triggers a clean
- * shutdown, solving the first item. To handle unclean shutdowns, we also wait
- * for EOF/HUP on both sides and quit if that signal is received.
- *
- * As an example, a shutdown might look as follows:
- *
- * - session-X.scope for user is stopped
- * - Leader process receives SIGTERM
- * - Leader sends single byte
- * - Monitor process receives byte and signals STOPPING=1
- * - Systemd user instance starts session teardown
- * - The last job that runs during session teardown is a stop job for the
- *   monitor process.
- * - Monitor process quits, closing FD in the process
- * - Leader process receives HUP and quits
- * - GDM sees the leader quit and cleans up its state in response.
- *
- * The result is that the session is stopped cleanly.
- */
+/* There is a lengthy comment in gnome-session/leader-systemd.c that
+   explains how this all works; I've removed it here for brevity */
 int
 main (int argc, char **argv)
 {
@@ -235,6 +115,7 @@ main (int argc, char **argv)
         if (!user)
                 user = "gdm-greeter"; // :/
         g_info("User is: %s", user);
+
         // strncmp because we also have gdm-greeter-{2,3,4,...}
         if (strncmp(user, "gdm-greeter", sizeof("gdm-greeter")) == 0)
         {
@@ -245,15 +126,13 @@ main (int argc, char **argv)
                 g_setenv("XDG_CONFIG_HOME", config_dir, TRUE);
                 g_setenv("HOME", home_dir, TRUE);
         }
-        else
-                g_warning("The gdm-greeter-{1,2,3,4} user wasn't found. Expect stuff to break.");
 
         // Finally, let's get started
         rc_set_user();
 
         char const *session_type = g_getenv("XDG_SESSION_TYPE");
         char const *home         = g_getenv("HOME");
-        g_info("XDG_RUNTIME_DIR: %s", g_getenv("XDG_RUNTIME_DIR"));
+        g_debug("XDG_RUNTIME_DIR: %s", g_getenv("XDG_RUNTIME_DIR"));
 
         // TODO what about custom XDG config directory?
         g_autofree char *gnome_runlevel_dir = g_strdup_printf("%s/.config/rc/runlevels/gnome-session", home);
@@ -265,9 +144,7 @@ main (int argc, char **argv)
         debug_string = g_getenv ("GNOME_SESSION_DEBUG");
         if (debug_string != NULL)
                 g_log_set_debug_enabled (atoi (debug_string) == 1);
-        else
-                g_log_set_debug_enabled(TRUE);
-        g_debug("Hi! from leader-openrc.");
+        g_debug("Hello from leader-openrc!");
 
         ctx.loop = g_main_loop_new (NULL, TRUE);
 
